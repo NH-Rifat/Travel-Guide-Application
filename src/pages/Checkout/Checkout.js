9364c0f3d4fc3ae27e032bbbedc9fb683fd67539
Hasan Rifat <hasanrifatr1@gmail.com>
2022-04-18 22:30:03 +0600

    checkout form are done

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -5,44 +5,41 @@ import bookImg from '../../images/book-img.svg';
 const Checkout = () => {
   return (
     <div className='book-container'>
-      <section class='book'>
-        <h1 class='heading'>
-          <span>b</span>
-          <span>o</span>
-          <span>o</span>
-          <span>k</span>
-          <span class='space'></span>
-          <span>n</span>
-          <span>o</span>
-          <span>w</span>
-        </h1>
-
-        <div class='row'>
-          <div class='image'>
-            <img src={bookImg} alt='' />
-          </div>
-
+    <h1 class="heading">
+        <span>b</span>
+        <span>o</span>
+        <span>o</span>
+        <span>k</span>
+        <span class="space"></span>
+        <span>n</span>
+        <span>o</span>
+        <span>w</span>
+    </h1>
+      <div className='booking'>
+        <div className='book-img'>
+          <img src={bookImg} alt='' />
+        </div>
+        <div className='form-container'>
           <form action=''>
-            <div class='inputBox'>
-              <h3>where to</h3>
-              <input type='text' placeholder='place name' />
+            <div className='info'>
+              <h3>Where to</h3>
+              <input type='text' placeholder='Place name' />
             </div>
-            <div class='inputBox'>
+            <div className='info'>
               <h3>how many</h3>
               <input type='number' placeholder='number of guests' />
             </div>
-            <div class='inputBox'>
+            <div className='info'>
               <h3>arrivals</h3>
               <input type='date' />
             </div>
-            <div class='inputBox'>
+            <div className='info'>
               <h3>leaving</h3>
               <input type='date' />
             </div>
-            <input type='submit' class='btn' value='Proceed checkout' />
           </form>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
