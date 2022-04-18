e92ae10f804fd1edea553323b0fd0aed92858080
Hasan Rifat <hasanrifatr1@gmail.com>
2022-04-18 22:55:09 +0600

    google login done

--- src/pages/Login/SocialLogin/SocialLogin.js
+++ src/pages/Login/SocialLogin/SocialLogin.js
@@ -4,28 +4,47 @@ import './SocialLogin.css';
 import facebook from '../../../images/facebook.png';
 import google from '../../../images/google.png';
 import github from '../../../images/github.png';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+import Loading from '../../Loading/Loading';
 
 const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  let errorElement;
+  if (loading) {
+    return <Loading></Loading>;
+  }
+  if (error) {
+    errorElement = <p className='text-danger'>Error: {error?.message}</p>;
+  }
+  if (user) {
+    navigate('/home');
+  }
   return (
-    <div className='social-login'>
-      <div className=''>
-        <div className='google social'>
-          <button>
-          <img src={google} alt="" />
-          <span>Google Sign in</span>
-          </button>
-        </div>
-        <div className='github social'>
-          <button>
-          <img src={github} alt="" />
-          <span>Github Sign in</span>
-          </button>
-        </div>
-        <div className='faceBook social'>
-          <button>
-          <img src={facebook} alt="" />
-          <span>Facebook Sign in</span>
-          </button>
+    <div className='social'>
+      <div className='social-login'>
+        <div className=''>
+          {errorElement}
+          <div className='google social'>
+            <button onClick={() => signInWithGoogle()}>
+              <img src={google} alt='' />
+              <span>Google Sign in</span>
+            </button>
+          </div>
+          <div className='github social'>
+            <button>
+              <img src={github} alt='' />
+              <span>Github Sign in</span>
+            </button>
+          </div>
+          <div className='faceBook social'>
+            <button>
+              <img src={facebook} alt='' />
+              <span>Facebook Sign in</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import './NotFound.css'
+
+import notFound from '../../images/404-error-page.jpg'
 
 const NotFound = () => {
   return (
     <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quae possimus voluptatibus distinctio saepe atque inventore impedit minima animi? Culpa modi quasi error nam laboriosam. Officiis consequuntur in rerum eveniet!
-      Itaque ipsa voluptatem minima, quibusdam quidem doloribus sequi natus architecto error iure fugiat magnam maiores modi possimus tempore. Est sequi rerum eum quaerat neque! Minima debitis nulla commodi fugiat possimus.
-      Illo provident ut tempora quasi eum dolorem nobis optio excepturi ea omnis officia sequi blanditiis repudiandae in praesentium, expedita facilis odit aperiam, unde quaerat. Facere ad nesciunt officiis molestiae consectetur?
-      Aperiam adipisci natus error cumque corrupti doloribus unde itaque debitis tempora magnam ea commodi, exercitationem nulla cum necessitatibus, veritatis ipsam explicabo? Inventore veniam deleniti temporibus autem porro, rerum repudiandae nihil.
-      Animi modi perferendis provident rerum quis? Autem esse aliquid ipsam neque. Impedit ipsam sunt, quae dolorem esse sint nesciunt earum possimus? Temporibus odio nihil, voluptate illo deserunt labore molestiae quae.
-      Placeat aliquid consectetur maiores numquam error sed, facere ullam fuga obcaecati deserunt delectus, sequi enim? Commodi recusandae quia eius obcaecati aspernatur minima ducimus. Assumenda minus reprehenderit, ex ipsa non eaque?</h1>
+      <div className="notfound">
+      <img src={notFound} alt="" />
+      </div>
     </div>
   );
 };
